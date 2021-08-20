32c7b4ad7d87c2a360a8c0aa23546952a843594d
cyascammer <putthipongpookhansung@kkumail.com>
2021-08-21 00:01:35 +0700

    lab5.py

---
+++ Putthipong-2243-lab5/p1.py
@@ -0,0 +1,15 @@
+'''
+Putthipong Phukhansung
+633040224-3
+problem 1
+'''
+
+import math
+
+
+def hypotenuse(n1, n2):
+    print("Sqrt({}^2 + {}^2) = {}".format(n1,n2,math.sqrt(pow(n1,2) + pow(n2,2))))
+
+hypotenuse(3.0,4.0)
+hypotenuse(3,4)
+hypotenuse(3,4.0)
