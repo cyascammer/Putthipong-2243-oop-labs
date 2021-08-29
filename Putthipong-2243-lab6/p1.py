34e6c27b77e9a8f14870110a48357000bf20552c
cyascammer <putthipongpookhansung@kkumail.com>
2021-08-29 15:05:27 +0700

    lab6.py

--- Putthipong-2243-lab6/p1.py
+++ Putthipong-2243-lab6/p1.py
@@ -5,5 +5,6 @@ for t in filenames1:
     filename = "file_00{:.0f}".format(t)
     filenames2.append(filename)
 
+
 print(str(filenames2[0]), "{:.2f}, {:1.2e}, {:1.2e}"
 .format(filenames1[1], filenames1[2], filenames1[3]))
